010242ac4e9eb385b1f0b67b6fb13c202ad49e67
Kevin Jones <kevin@vcsjones.com>
2015-10-27 10:00:35 -0400

    Added error handling when scripts execute. If a script throws an exception, Fiddler will no longer crash.

---
+++ VCSJones.FiddlerScriptCSharp/FiddlerCSharpScript.cs
@@ -0,0 +1,86 @@
+using System;
+using System.IO;
+using Fiddler;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.CodeAnalysis.Scripting.CSharp;
+
+namespace VCSJones.FiddlerScriptCSharp
+{
+    public class FiddlerCSharpScript : IEquatable<FiddlerCSharpScript>, IComparable<FiddlerCSharpScript>
+    {
+        private readonly string _path;
+        private Action<Session> AutoTamperRequestBeforeDelegate, AutoTamperRequestAfterDelegate, AutoTamperResponseAfterDelegate, AutoTamperResponseBeforeDelegate;
+        private Action<Session> OnPeekAtRequestHeadersDelegate, OnPeekAtResponseHeadersDelegate;
+        private Action<Session> OnBeforeReturningErrorDelegate;
+        private Action OnFiddlerAttachDelegate, OnFiddlerDetachDelegate;
+        private Func<bool> OnFiddlerBeforeShutdownDelegate;
+        private Action OnFiddlerBootDelegate, OnFiddlerShutdownDelegate;
+
+
+        public FiddlerCSharpScript(string path)
+        {
+            _path = path;
+        }
+
+        public void Initialize()
+        {
+            var options = ScriptOptions.Default
+                .WithIsInteractive(false)
+                .AddSearchPaths(CONFIG.GetPath("App"), CONFIG.GetPath("Scripts"))
+                .AddReferences("mscorlib.dll", "System.dll", "System.Core.dll", "Microsoft.CSharp.dll", "Fiddler.exe")
+                .AddNamespaces("Fiddler");
+            string text;
+            using (var fs = new FileStream(_path, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = new StreamReader(fs))
+                {
+                    text = reader.ReadToEnd();
+                }
+            }
+
+            ScriptState script;
+            try
+            {
+                script = CSharpScript.Run(text, options);
+            }
+            catch (Exception e)
+            {
+                FiddlerApplication.Log.LogString(e.ToString());
+                return;
+            }
+            script.CreateDelegate<Action>("Main")?.Invoke();
+            AutoTamperRequestBeforeDelegate = script.CreateDelegate<Action<Session>>("OnBeforeRequest") ?? script.CreateDelegate<Action<Session>>(nameof(IAutoTamper3.AutoTamperRequestBefore));
+            AutoTamperRequestAfterDelegate = script.CreateDelegate<Action<Session>>("OnAfterRequest") ?? script.CreateDelegate<Action<Session>>(nameof(IAutoTamper3.AutoTamperRequestAfter));
+            AutoTamperResponseBeforeDelegate = script.CreateDelegate<Action<Session>>("OnBeforeResponse") ?? script.CreateDelegate<Action<Session>>(nameof(IAutoTamper3.AutoTamperResponseBefore));
+            AutoTamperResponseAfterDelegate = script.CreateDelegate<Action<Session>>("OnAfterResponse") ?? script.CreateDelegate<Action<Session>>(nameof(IAutoTamper3.AutoTamperResponseAfter));
+            OnBeforeReturningErrorDelegate = script.CreateDelegate<Action<Session>>("OnReturningError") ?? script.CreateDelegate<Action<Session>>(nameof(IAutoTamper3.OnBeforeReturningError));
+            OnPeekAtRequestHeadersDelegate = script.CreateDelegate<Action<Session>>(nameof(IAutoTamper3.OnPeekAtRequestHeaders));
+            OnPeekAtResponseHeadersDelegate = script.CreateDelegate<Action<Session>>(nameof(IAutoTamper3.OnPeekAtResponseHeaders));
+            OnFiddlerAttachDelegate = script.CreateDelegate<Action>("OnAttach");
+            OnFiddlerDetachDelegate = script.CreateDelegate<Action>("OnDetach");
+            OnFiddlerBeforeShutdownDelegate = script.CreateDelegate<Func<bool>>("OnBeforeShutdown");
+            OnFiddlerBootDelegate = script.CreateDelegate<Action>("OnBoot");
+            OnFiddlerShutdownDelegate = script.CreateDelegate<Action>("OnShutdown");
+        }
+
+        public int CompareTo(FiddlerCSharpScript other) => StringComparer.OrdinalIgnoreCase.Compare(_path, other._path);
+
+        public bool Equals(FiddlerCSharpScript other) =>  CompareTo(other) == 0;
+
+        public void AutoTamperRequestBefore(Session session) => AutoTamperRequestBeforeDelegate?.Invoke(session);
+        public void AutoTamperRequestAfter(Session session) => AutoTamperRequestAfterDelegate?.Invoke(session);
+        public void AutoTamperResponseAfter(Session session) => AutoTamperResponseAfterDelegate?.Invoke(session);
+        public void AutoTamperResponseBefore(Session session) => AutoTamperResponseBeforeDelegate?.Invoke(session);
+        public void OnPeekAtResponseHeaders(Session session) => OnPeekAtResponseHeadersDelegate?.Invoke(session);
+        public void OnPeekAtRequestHeaders(Session session) => OnPeekAtRequestHeadersDelegate?.Invoke(session);
+        public void OnBeforeReturningError(Session session) => OnBeforeReturningErrorDelegate?.Invoke(session);
+
+        public void OnBoot() => OnFiddlerBootDelegate?.Invoke();
+        public void OnShutdown() => OnFiddlerShutdownDelegate?.Invoke();
+        public bool? OnBeforeShutdown() => OnFiddlerBeforeShutdownDelegate?.Invoke();
+        public void OnAttach() => OnFiddlerAttachDelegate?.Invoke();
+        public void OnDetach() => OnFiddlerDetachDelegate?.Invoke();
+
+        public override int GetHashCode() => _path.GetHashCode();
+    }
+}
--- VCSJones.FiddlerScriptCSharp/ScriptCSharpRepository.cs
+++ VCSJones.FiddlerScriptCSharp/ScriptCSharpRepository.cs
@@ -1,14 +1,9 @@
 ﻿using Fiddler;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Scripting;
-using Microsoft.CodeAnalysis.Scripting.CSharp;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Reactive;
 using System.Reactive.Linq;
-using System.Reflection;
 
 namespace VCSJones.FiddlerScriptCSharp
 {
@@ -60,11 +55,36 @@ namespace VCSJones.FiddlerScriptCSharp
             }
         }
 
+        private static void ExecuteSafely(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                FiddlerApplication.Log.LogString($"An exception occured while executing part of a script:\r\n{e}");
+            }
+        }
+
+        private static T ExecuteSafely<T>(Func<T> action)
+        {
+            try
+            {
+                return action();
+            }
+            catch (Exception e)
+            {
+                FiddlerApplication.Log.LogString($"An exception occured while executing part of a script:\r\n{e}");
+                return default(T);
+            }
+        }
+
         public void ExecuteAllAutoTamperRequestBefore(Session session)
         {
             foreach (var script in _activeScripts.Values)
             {
-                script.AutoTamperRequestBefore(session);
+                ExecuteSafely(() => script.AutoTamperRequestBefore(session));
             }
         }
 
@@ -72,7 +92,7 @@ namespace VCSJones.FiddlerScriptCSharp
         {
             foreach (var script in _activeScripts.Values)
             {
-                script.AutoTamperRequestAfter(session);
+                ExecuteSafely(() => script.AutoTamperRequestAfter(session));
             }
         }
 
@@ -80,7 +100,7 @@ namespace VCSJones.FiddlerScriptCSharp
         {
             foreach (var script in _activeScripts.Values)
             {
-                script.AutoTamperResponseAfter(session);
+                ExecuteSafely(() => script.AutoTamperResponseAfter(session));
             }
         }
 
@@ -88,7 +108,7 @@ namespace VCSJones.FiddlerScriptCSharp
         {
             foreach (var script in _activeScripts.Values)
             {
-                script.AutoTamperResponseBefore(session);
+                ExecuteSafely(() => script.AutoTamperResponseBefore(session));
             }
         }
 
@@ -96,7 +116,7 @@ namespace VCSJones.FiddlerScriptCSharp
         {
             foreach (var script in _activeScripts.Values)
             {
-                script.OnPeekAtRequestHeaders(session);
+                ExecuteSafely(() => script.OnPeekAtRequestHeaders(session));
             }
         }
 
@@ -105,7 +125,7 @@ namespace VCSJones.FiddlerScriptCSharp
         {
             foreach (var script in _activeScripts.Values)
             {
-                script.OnPeekAtResponseHeaders(session);
+                ExecuteSafely(() => script.OnPeekAtResponseHeaders(session));
             }
         }
 
@@ -113,7 +133,7 @@ namespace VCSJones.FiddlerScriptCSharp
         {
             foreach (var script in _activeScripts.Values)
             {
-                script.OnBeforeReturningError(session);
+                ExecuteSafely(() => script.OnBeforeReturningError(session));
             }
         }
 
@@ -121,7 +141,7 @@ namespace VCSJones.FiddlerScriptCSharp
         {
             foreach (var script in _activeScripts.Values)
             {
-                script.OnBoot();
+                ExecuteSafely(() => script.OnBoot());
             }
         }
 
@@ -129,7 +149,7 @@ namespace VCSJones.FiddlerScriptCSharp
         {
             foreach (var script in _activeScripts.Values)
             {
-                script.OnAttach();
+                ExecuteSafely(() => script.OnAttach());
             }
         }
 
@@ -137,7 +157,7 @@ namespace VCSJones.FiddlerScriptCSharp
         {
             foreach (var script in _activeScripts.Values)
             {
-                script.OnDetach();
+                ExecuteSafely(() => script.OnDetach());
             }
         }
 
@@ -145,16 +165,16 @@ namespace VCSJones.FiddlerScriptCSharp
         {
             foreach (var script in _activeScripts.Values)
             {
-                script.OnShutdown();
+                ExecuteSafely(() => script.OnShutdown());
             }
         }
 
         public bool ExecuteAllOnBeforeShutdown()
         {
-            bool continueShutDown = true;
+            var continueShutDown = true;
             foreach (var script in _activeScripts.Values)
             {
-                continueShutDown &= script.OnBeforeShutdown() ?? true;
+                continueShutDown &= ExecuteSafely(() => script.OnBeforeShutdown()) ?? true;
             }
             return continueShutDown;
         }
@@ -174,89 +194,10 @@ namespace VCSJones.FiddlerScriptCSharp
             }
         }
 
-
         public void Dispose()
         {
             _watcher.EnableRaisingEvents = false;
             _watcher.Dispose();
         }
-    }
-
-    public class FiddlerCSharpScript : IEquatable<FiddlerCSharpScript>, IComparable<FiddlerCSharpScript>
-    {
-        private readonly string _path;
-        private Action<Session> AutoTamperRequestBeforeDelegate, AutoTamperRequestAfterDelegate, AutoTamperResponseAfterDelegate, AutoTamperResponseBeforeDelegate;
-        private Action<Session> OnPeekAtRequestHeadersDelegate, OnPeekAtResponseHeadersDelegate;
-        private Action<Session> OnBeforeReturningErrorDelegate;
-        private Action OnFiddlerAttachDelegate, OnFiddlerDetachDelegate;
-        private Func<bool> OnFiddlerBeforeShutdownDelegate;
-        private Action OnFiddlerBootDelegate, OnFiddlerShutdownDelegate;
-
-
-        public FiddlerCSharpScript(string path)
-        {
-            _path = path;
-        }
-
-        public void Initialize()
-        {
-            var options = ScriptOptions.Default
-                            .WithIsInteractive(false)
-                            .AddSearchPaths(CONFIG.GetPath("App"), CONFIG.GetPath("Scripts"))
-                            .AddReferences("mscorlib.dll", "System.dll", "System.Core.dll", "Microsoft.CSharp.dll", "Fiddler.exe")
-                            .AddNamespaces("Fiddler");
-            string text;
-            using (var fs = new FileStream(_path, FileMode.Open, FileAccess.Read))
-            {
-                using (var reader = new StreamReader(fs))
-                {
-                    text = reader.ReadToEnd();
-                }
-            }
-
-            ScriptState script;
-            try
-            {
-                script = CSharpScript.Run(text, options);
-            }
-            catch (Exception e)
-            {
-                FiddlerApplication.Log.LogString(e.ToString());
-                return;
-            }
-            script.CreateDelegate<Action>("Main")?.Invoke();
-            AutoTamperRequestBeforeDelegate = script.CreateDelegate<Action<Session>>("OnBeforeRequest") ?? script.CreateDelegate<Action<Session>>(nameof(IAutoTamper3.AutoTamperRequestBefore));
-            AutoTamperRequestAfterDelegate = script.CreateDelegate<Action<Session>>("OnAfterRequest") ?? script.CreateDelegate<Action<Session>>(nameof(IAutoTamper3.AutoTamperRequestAfter));
-            AutoTamperResponseBeforeDelegate = script.CreateDelegate<Action<Session>>("OnBeforeResponse") ?? script.CreateDelegate<Action<Session>>(nameof(IAutoTamper3.AutoTamperResponseBefore));
-            AutoTamperResponseAfterDelegate = script.CreateDelegate<Action<Session>>("OnAfterResponse") ?? script.CreateDelegate<Action<Session>>(nameof(IAutoTamper3.AutoTamperResponseAfter));
-            OnBeforeReturningErrorDelegate = script.CreateDelegate<Action<Session>>("OnReturningError") ?? script.CreateDelegate<Action<Session>>(nameof(IAutoTamper3.OnBeforeReturningError));
-            OnPeekAtRequestHeadersDelegate = script.CreateDelegate<Action<Session>>(nameof(IAutoTamper3.OnPeekAtRequestHeaders));
-            OnPeekAtResponseHeadersDelegate = script.CreateDelegate<Action<Session>>(nameof(IAutoTamper3.OnPeekAtResponseHeaders));
-            OnFiddlerAttachDelegate = script.CreateDelegate<Action>("OnAttach");
-            OnFiddlerDetachDelegate = script.CreateDelegate<Action>("OnDetach");
-            OnFiddlerBeforeShutdownDelegate = script.CreateDelegate<Func<bool>>("OnBeforeShutdown");
-            OnFiddlerBootDelegate = script.CreateDelegate<Action>("OnBoot");
-            OnFiddlerShutdownDelegate = script.CreateDelegate<Action>("OnShutdown");
-        }
-
-        public int CompareTo(FiddlerCSharpScript other) => StringComparer.OrdinalIgnoreCase.Compare(_path, other._path);
-
-        public bool Equals(FiddlerCSharpScript other) =>  CompareTo(other) == 0;
-
-        public void AutoTamperRequestBefore(Session session) => AutoTamperRequestBeforeDelegate?.Invoke(session);
-        public void AutoTamperRequestAfter(Session session) => AutoTamperRequestAfterDelegate?.Invoke(session);
-        public void AutoTamperResponseAfter(Session session) => AutoTamperResponseAfterDelegate?.Invoke(session);
-        public void AutoTamperResponseBefore(Session session) => AutoTamperResponseBeforeDelegate?.Invoke(session);
-        public void OnPeekAtResponseHeaders(Session session) => OnPeekAtResponseHeadersDelegate?.Invoke(session);
-        public void OnPeekAtRequestHeaders(Session session) => OnPeekAtRequestHeadersDelegate?.Invoke(session);
-        public void OnBeforeReturningError(Session session) => OnBeforeReturningErrorDelegate?.Invoke(session);
-
-        public void OnBoot() => OnFiddlerBootDelegate?.Invoke();
-        public void OnShutdown() => OnFiddlerShutdownDelegate?.Invoke();
-        public bool? OnBeforeShutdown() => OnFiddlerBeforeShutdownDelegate?.Invoke();
-        public void OnAttach() => OnFiddlerAttachDelegate?.Invoke();
-        public void OnDetach() => OnFiddlerDetachDelegate?.Invoke();
-
-        public override int GetHashCode() => _path.GetHashCode();
     }
 }
